5e2dd3a620700704ecc7ee2b3bf800bd76840174
Markus Bucher <markus.bucher@she.net>
2012-08-02 14:22:21 +0200

    [BUGFIX] Changing secondCar to theCar MotorCycle != Car

--- src/main/Start.java
+++ src/main/Start.java
@@ -6,8 +6,8 @@ import cars.MotorCycle;
 public class Start {
 	
 	public static void main(String[] args) {
-		MotorCycle motorCycle = new Car();
-		Car secondCar = new Car();
+		MotorCycle motorCycle = new MotorCycle();
+		Car theCar = new Car();
 		
 		//input
 		motorCycle.setManufacturer("Honda");
@@ -19,15 +19,15 @@ public class Start {
 		motorCycle.setSpeed(171);
 		motorCycle.setPrice(2000);
 		
-		secondCar.setManufacturer("Ford");
-		secondCar.setModel("Focus");
-		secondCar.setColor("blue");
-		secondCar.setGasolineType("Benzin");
-		secondCar.setDoors(4);
-		secondCar.setConsumptionBoth((float)8);
-		secondCar.setPs(130);
-		secondCar.setSpeed(182);
-		secondCar.setPrice(5000);
+		theCar.setManufacturer("Ford");
+		theCar.setModel("Focus");
+		theCar.setColor("blue");
+		theCar.setGasolineType("Benzin");
+		theCar.setDoors(4);
+		theCar.setConsumptionBoth((float)8);
+		theCar.setPs(130);
+		theCar.setSpeed(182);
+		theCar.setPrice(5000);
 		
 		//output
 		System.out.println("motorcycle:");
@@ -50,20 +50,20 @@ public class Start {
 		
 		System.out.println("car:");
 		System.out.println("_________________");
-		System.out.println("Manufacturer: " + secondCar.getManufacturer());
-		System.out.println("Model: " + secondCar.getModel());
-		System.out.println("gasoline type: " + secondCar.getGasolineType());
-		System.out.println("color: " + secondCar.getColor());
-		System.out.println("doors: " + secondCar.getDoors());
-		if (secondCar.getConsumptionInTowns() != 0)
-			System.out.println("consumption in Towns: " + secondCar.getConsumptionInTowns());
-		if(secondCar.getConsumptionOutOfTowns() != 0)
-			System.out.println("consumption out of Towns: " + secondCar.getConsumptionOutOfTowns());
-		if (secondCar.getConsumptionBoth() != 0)
-			System.out.println("consumption both: " + secondCar.getConsumptionBoth());
-		System.out.println("ps: " + secondCar.getPs());
-		System.out.println("speed: " + secondCar.getSpeed());
-		System.out.println("price: " + secondCar.getPrice());
+		System.out.println("Manufacturer: " + theCar.getManufacturer());
+		System.out.println("Model: " + theCar.getModel());
+		System.out.println("gasoline type: " + theCar.getGasolineType());
+		System.out.println("color: " + theCar.getColor());
+		System.out.println("doors: " + theCar.getDoors());
+		if (theCar.getConsumptionInTowns() != 0)
+			System.out.println("consumption in Towns: " + theCar.getConsumptionInTowns());
+		if(theCar.getConsumptionOutOfTowns() != 0)
+			System.out.println("consumption out of Towns: " + theCar.getConsumptionOutOfTowns());
+		if (theCar.getConsumptionBoth() != 0)
+			System.out.println("consumption both: " + theCar.getConsumptionBoth());
+		System.out.println("ps: " + theCar.getPs());
+		System.out.println("speed: " + theCar.getSpeed());
+		System.out.println("price: " + theCar.getPrice());
 	}
 	
 }
